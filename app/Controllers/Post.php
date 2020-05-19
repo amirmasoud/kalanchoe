2a3d694dda0ca084e5be22637f721688d60a67d2
Amirmasoud <amirmasood33@gmail.com>
2020-05-19 09:28:35 +0430

    Change shadow on hover

--- app/Controllers/Post.php
+++ app/Controllers/Post.php
@@ -8,6 +8,6 @@ class Post extends Controller
 {
     public static function thumbnail()
     {
-        return the_post_thumbnail('post-thumbnail', ['class' => 'mb-4 object-cover h-40 w-full rounded shadow-md']);
+        return the_post_thumbnail('post-thumbnail', ['class' => 'mb-4 object-cover h-40 w-full rounded shadow-md hover:shadow-lg']);
     }
 }
