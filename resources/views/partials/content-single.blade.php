32d98eaece5696e5cc1d7de0bf070c69a8937a38
Amirmasoud <amirmasood33@gmail.com>
2020-05-24 11:18:20 +0430

    Many tags

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,4 +1,5 @@
 <article @php post_class('mx-4') @endphp>
+  @if (has_post_thumbnail())
   <header class="flex flex-wrap flex-col-reverse md:flex-row justify-between mb-20">
     <div class="w-full md:w-1/2">
       <div class="w-full mb-4">@include('partials/category-all')</div>
@@ -9,13 +10,24 @@
     </div>
     {!! Post::thumbnail('w-full md:w-1/2 h-full') !!}
   </header>
+  @else
+  <header class="flex flex-wrap justify-between mb-20">
+    <div class="w-full">
+      <div class="w-full mb-4">@include('partials/category-all')</div>
+      <h1 class="entry-title text-5xl md:mr-12">{!! get_the_title() !!}</h1>
+      <div class="entry-excerpt mt-12 md:mr-12 text-lg">
+        @php the_excerpt() @endphp
+      </div>
+    </div>
+  </header>
+  @endif
   <div class="entry-content text-lg">
     @php the_content() @endphp
   </div>
   <footer class="flex flex-wrap max-w-3xl mx-auto">
     <div class="flex w-full justify-between">
-      @include('partials/tag-all')
-      @include('partials/entry-time')
+      <div class="w-full md:w-2/3">@include('partials/tag-all')</div>
+      <div class="w-full md:w-1/3 text-right">@include('partials/entry-time')</div>
     </div>
     <div class="w-full">
       {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
