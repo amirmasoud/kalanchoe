2d5e69aa7c1717b98d9f96c875ea511b1c11393b
Amirmasoud <amirmasood33@gmail.com>
2020-06-07 23:38:44 +0430

    gallery flex display 1 to 10

--- resources/assets/styles/tailwind.config.js
+++ resources/assets/styles/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
         serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
       },
+      width: {
+        "1/7": "14.28571429%",
+        "1/8": "12.5%",
+        "1/9": "11.1111111111%",
+        "1/10": "10%",
+      },
     },
   },
   variants: {},
