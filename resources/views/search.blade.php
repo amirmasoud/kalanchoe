e07c323872503facc9062c3340b614c459618ce8
Amirmasoud <amirmasood33@gmail.com>
2020-06-21 20:00:04 +0430

    wip

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,20 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  <div class="container mx-auto my-4">
+    @include('partials.page-header')
 
-  @if (!have_posts())
+    @if (!have_posts())
     <div class="alert alert-warning px-4 my-4">
       {{ __('Sorry, no results were found.', 'sage') }}
     </div>
     <div class="m-4">
       {!! get_search_form(false) !!}
     </div>
-  @endif
+    @endif
+  </div>
 
+  <div class="flex flex-wrap container mx-auto">
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-search')
   @endwhile
 
   {!! get_the_posts_navigation() !!}
+  </div>
 @endsection
