a020a4b9ed1cf51a07a54aa04cd1bc972ce3a5bd
Amirmasoud <amirmasood33@gmail.com>
2020-06-21 21:35:00 +0430

    404

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  <div class="container mx-auto">
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+    <div class="text-center text-4xl">
+      @include('partials.page-header')
     </div>
-    {!! get_search_form(false) !!}
-  @endif
+
+    <div class="px-4 my-16 text-center">
+      @if (!have_posts())
+        <div class="alert alert-warning">
+          {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
+        </div>
+        <div class="w-48 mx-auto my-8">
+          {!! get_search_form(false) !!}
+        </div>
+      @endif
+    </div>
+  </div>
 @endsection
