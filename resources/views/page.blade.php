ae4cffde6b75d2a811ef01d271faa5e9ad8cf8e3
Amirmasoud <amirmasood33@gmail.com>
2020-06-21 21:38:58 +0430

    page

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
+  <div class="container mx-auto px-4 mb-16">
+    @while(have_posts()) @php the_post() @endphp
+      @include('partials.page-header')
+      @include('partials.content-page')
+    @endwhile
+  </div>
 @endsection
