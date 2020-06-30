86690e5757d7d76e3155df3583bb881a7084b842
Amirmasoud <amirmasood33@gmail.com>
2020-06-30 22:34:01 +0430

    Padded search area

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -5,7 +5,7 @@
         <a class="w-full md:w-auto text-center brand font-serif font-bold text-3xl text-black" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
         <span class="w-full md:w-auto text-center italic font-light text-sm">{{ get_bloginfo('description', 'display') }}</span>
       </div>
-      <div class="flex flex-wrap flex-col md:flex-no-wrap md:flex-row justify-center md:justify-end align-center w-full mt-8 md:w-1/2 md:mt-0">
+      <div class="flex flex-wrap flex-col md:flex-no-wrap md:flex-row justify-center md:justify-end align-center w-full mt-8 px-4 md:w-1/2 md:mt-0">
         <div class="w-auto mx-auto md:mx-0 md:mb-0 mb-4">{{ get_search_form() }}</div>
         <nav class="nav-social flex mx-auto md:mx-0 md:mb-8 items-center mb-4">
           @if (has_nav_menu('social_navigation'))
