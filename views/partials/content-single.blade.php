fe00135615e07a37b9af250976ae76d95ec8b0d7
Amirmasoud <amirmasood33@gmail.com>
2020-07-19 23:49:38 +0430

    WIP

--- views/partials/content-single.blade.php
+++ views/partials/content-single.blade.php
@@ -1,4 +1,4 @@
-<article @php post_class('container px-4 prose') @endphp>
+<article @php post_class('px-4 prose mx-auto max-w-3xl') @endphp>
   @if (has_post_thumbnail())
   <header class="flex flex-wrap mx-auto flex-col-reverse md:flex-row justify-between mb-20">
     <div class="w-full md:w-1/2">
@@ -21,17 +21,19 @@
     </div>
   </header>
   @endif
-  <div class="entry-content flex flex-wrap mx-auto">
-    @php the_content() @endphp
+  <div class="container mx-auto">
+    <div class="entry-content flex flex-wrap mx-auto">
+      @php the_content() @endphp
+    </div>
+    <footer class="flex flex-wrap mx-auto">
+      <div class="flex w-full">
+        {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+      </div>
+      <div class="flex w-full justify-between mx-4">
+        <div class="w-full md:w-2/3">@include('partials/tag-all')</div>
+        <div class="w-full md:w-1/3 text-right">@include('partials/entry-time')</div>
+      </div>
+    </footer>
+    @php comments_template('/partials/comments.blade.php') @endphp
   </div>
-  <footer class="flex flex-wrap mx-auto">
-    <div class="flex w-full">
-      {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-    </div>
-    <div class="flex w-full justify-between mx-4">
-      <div class="w-full md:w-2/3">@include('partials/tag-all')</div>
-      <div class="w-full md:w-1/3 text-right">@include('partials/entry-time')</div>
-    </div>
-  </footer>
-  @php comments_template('/partials/comments.blade.php') @endphp
 </article>
