bfaa7a53edb50be9db25ecdc240e5ae086ad550a
Amirmasoud <amirmasood33@gmail.com>
2020-07-21 22:46:47 +0430

    Fix submenu

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <body @php body_class() @endphp>
     @php do_action('get_header') @endphp
     @include('partials.header')
-    <div class="wrap mx-auto" role="document">
+    <div class="wrap mx-auto mt-32" role="document">
       <div class="content">
         <main class="main">
           @yield('content')
