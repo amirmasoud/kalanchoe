cb579ad5e02ca7abe6b57abb93715f0aa7c4ede5
Amirmasoud <amirmasood33@gmail.com>
2020-07-26 21:36:40 +0430

    More width for content header

--- views/partials/content-single.blade.php
+++ views/partials/content-single.blade.php
@@ -1,6 +1,6 @@
 <article @php post_class('px-4 mx-auto') @endphp>
   @if (has_post_thumbnail())
-  <header class="flex flex-wrap prose mx-auto flex-col-reverse md:flex-row justify-between mb-20">
+  <header class="flex flex-wrap prose max-w-3xl mx-auto flex-col-reverse md:flex-row justify-between mb-20">
     <div class="w-full md:w-1/2">
       <div class="w-full mb-4">@include('partials/category-all')</div>
       <h1 class="entry-title text-5xl md:mr-12 break-words">{!! get_the_title() !!}</h1>
@@ -11,7 +11,7 @@
     {!! Post::thumbnail('single-thumbnail') !!}
   </header>
   @else
-  <header class="flex flex-wrap prose mx-auto justify-between mb-20">
+  <header class="flex flex-wrap prose max-w-3xl mx-auto justify-between mb-20">
     <div class="w-full">
       <div class="w-full mb-4">@include('partials/category-all')</div>
       <h1 class="entry-title text-5xl md:mr-12 break-words">{!! get_the_title() !!}</h1>
